1848ce059b5508851c73338471b4d7b40bb36a09
Kayajeet Bora <kavyajeet.bora@burohappold.com>
2024-11-22 17:08:43 +0530

    Add - mount streamlit volumes

--- app/app.py
+++ app/app.py
@@ -1,34 +1,30 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 from datetime import datetime
-import time
+from streamlit_autorefresh import st_autorefresh
 
 # global variables
-csv_path = r"../opt/airflow/config/data/weather.csv"
-refresh_time = 20
+csv_path = r"..\config\data\weather.csv"
 
 # Streamlit app header
 st.header("Weather for Top 25 Cities in India 🇮🇳", divider="gray")
 
 
-@st.cache_data(ttl=refresh_time)
-def load_data(csv_path):
-    df = pd.read_csv(csv_path)
+def load_data(path):
+    df = pd.read_csv(path)
     return df
 
 
-@st.cache_data(ttl=refresh_time)
-def plot_city_temperatures(df):
+def plot_city_temperatures(data):
     """
     Plots the temperature of cities from the given DataFrame.
 
     df (DataFrame): DataFrame containing city names and their corresponding temperatures.
     """
     fig, ax = plt.subplots(figsize=(12, 6))  # Create a figure and axis
-
     # Create a bar graph
-    bars = ax.bar(df["City"], df["temp"], color="skyblue")
+    bars = ax.bar(data["City"], data["temp"], color="skyblue")
 
     # Add titles and labels
     ax.set_title("Temperature of Cities in India", fontsize=16)
@@ -36,8 +32,8 @@ def plot_city_temperatures(df):
     ax.set_ylabel("Temperature (°C)", fontsize=14)
 
     # Rotate x-titles for better visibility
-    ax.set_xticks(range(len(df["City"])))  # Set x-ticks to match the number of cities
-    ax.set_xticklabels(df["City"], rotation=45, ha="right")
+    ax.set_xticks(range(len(data["City"])))  # Set x-ticks to match the number of cities
+    ax.set_xticklabels(data["City"], rotation=45, ha="right")
 
     # Add grid for better readability
     ax.yaxis.grid(True, linestyle="--", alpha=0.7)
@@ -53,26 +49,26 @@ def plot_city_temperatures(df):
             va="bottom",
             fontsize=10,
         )  # Add text above the bar
-
-    # Adjust layout to prevent clipping of tick-labels
     plt.tight_layout()
-
     return fig
 
 
+# update every 5 seconds
+refreshed = st_autorefresh(interval=5 * 1000, key="dataframerefresh")
+
 ## Load the data
+# global variables
+csv_path = r"..\config\data\weather.csv"
+
 df = load_data(csv_path)
 
-## Plot with the updated data
-fig = plot_city_temperatures(df)
-
 # Display the last updated time
-current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+current_time = datetime.now().strftime("%Y-%m-%d | %H:%M:%S")
 update_time = st.markdown(
     f"<p style='font-size: 14px;'>Last updated: {current_time}</p>",
     unsafe_allow_html=True,
 )
 # Render the plot in application
-updated_plot = st.pyplot(fig)
-
-time.sleep(refresh_time)
+## Plot with the updated data
+fig = plot_city_temperatures(df)
+st.pyplot(fig)
